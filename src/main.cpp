90d13b2b31316f50ad803f51441154b1c6b9dc05
candyPz <candy_pz@outlook.com>
2017-07-31 00:04:43 +0800

    add libevent

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <iostream>
+#include <event.h>
 #include "log/EtLog.h"
 
+void onTime(int sock, short event, void *arg) {
+    std::cout << "@@@@" << std::endl;
+
+    struct timeval tv;
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+
+    event_add((struct event*)arg, &tv);
+}
+
 int main() {
-    std::cout << "111" << std::endl;
+
+    event_init();
+
+    struct event evTime;
+    evtimer_set(&evTime, onTime, &evTime);
+
+    struct timeval tv;
+    tv.tv_sec = 1;
+    tv.tv_usec = 0;
+
+    event_add(&evTime, &tv);
+
+    event_dispatch();
 
     return 0;
 }
