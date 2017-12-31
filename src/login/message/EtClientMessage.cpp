9462ce837dd3e906c9e1f7f825135412fdfcd092
candyPz <candy_pz@outlook.com>
2017-12-31 17:32:46 +0800

    修改 协议

--- src/login/message/EtClientMessage.cpp
+++ src/login/message/EtClientMessage.cpp
@@ -5,6 +5,9 @@ CEtClientMessage::CEtClientMessage(evutil_socket_t fd, const char *data, size_t 
     m_fd = fd;
     m_head = new CMessage();
     m_head->ParseFromArray(data, len);
+    CRegistration_Req _content;
+    _content.ParseFromString(m_head->content());
+    LOG_CRIT("{0} {1} {2} {3}", m_head->sn(), m_head->cmd(), _content.account(), _content.password());
 }
 
 CEtClientMessage::~CEtClientMessage() {
